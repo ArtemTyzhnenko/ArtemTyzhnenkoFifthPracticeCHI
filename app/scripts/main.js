b80b161dcdc92d5d6ad24c86d7b39db1f3d66692
ArtemTyzhnenko <tyzhnenkoartem@gmail.com>
2017-11-20 17:50:43 +0200

    added hamburger and fix mistakes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+  if ($(window).width() < 540) {
+  $(".cross").hide();
+  $(".navigation").hide();
+  $(".hamburger").click(function () {
+    $(".navigation").slideToggle("slow", function () {
+      $(".hamburger").hide();
+      $(".cross").show();
+    });
+  });
+  $( ".cross" ).click(function() {
+    $( ".navigation" ).slideToggle( "slow", function() {
+      $( ".cross" ).hide();
+      $( ".hamburger" ).show();
+    });
+  });
+  }
+});
+
+
